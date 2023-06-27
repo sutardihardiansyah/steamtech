a436f87d00296a4481b838fd8af6fe7f9c8136c9
Sutardi Hardiansyah <sutardi@xeranta.com>
2023-06-27 11:28:36 +0700

    Update testimoni, gallery dan banner

--- assets/js/main.js
+++ assets/js/main.js
@@ -122,13 +122,6 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   /**
-   * Initiate glightbox
-   */
-  const glightbox = GLightbox({
-    selector: ".glightbox",
-  });
-
-  /**
    * Scroll top button
    */
   const scrollTop = document.querySelector(".scroll-top");
